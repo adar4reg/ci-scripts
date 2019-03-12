d7da51bca2340b1204f9dc722158b5cb25fdabd4
maksonlee(41110) <maksonlee@arimacomm.com.tw>
2019-03-12 11:11:09 +0800

    Refactor tradefed test script
    Change-Id: I24ff336b640bf3d0d77fca21358bc32fac2854e3

--- autotest/tradefed/tradefed.rb
+++ autotest/tradefed/tradefed.rb
@@ -2,6 +2,18 @@ require 'rest-client'
 require 'json'
 require 'digest'
 
+def download(url, file)
+  puts "download #{file} ..."
+  result = RestClient.get url
+  File.write(file, result.to_s)
+end
+
+def upload(url, file)
+  puts "upload #{file} ..."
+  md5 = Digest::MD5.hexdigest File.read(file)
+  RestClient.put url, File.open(file, 'rb'), {"X-Checksum": md5}
+end
+
 project = ARGV[0]
 
 base_url = "http://artifactory.arimacomm.com.tw:8081/artifactory"
@@ -14,19 +26,15 @@ files.each do |file|
     fastboot_artifact = file['name']
     autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'autotest.zip')
     RestClient.get "#{base_url}/api/storage/libs-test-local/#{file['path']}/#{autotest_artifact}"
-  rescue
-    puts "download #{fastboot_artifact} ..."
-    result = RestClient.get "#{base_url}/#{file['repo']}/#{file['path']}/#{fastboot_artifact}"
-    File.write(fastboot_artifact, result.to_s)
+  rescue    
+    download("#{base_url}/#{file['repo']}/#{file['path']}/#{fastboot_artifact}", fastboot_artifact)
 
     system("./tradefed.sh", "#{project}", "#{fastboot_artifact}", "#{autotest_artifact}")
-    
+
     if $?.exitstatus > 0
       exit($?.exitstatus)
     end
-
-    puts "upload #{autotest_artifact} ..."
-    md5 = Digest::MD5.hexdigest File.read(autotest_artifact)
-    RestClient.put "#{base_url}/libs-test-local/#{file['path']}/#{autotest_artifact}", File.open(autotest_artifact, 'rb'), {"X-Checksum": md5}
+    
+    upload("#{base_url}/libs-test-local/#{file['path']}/#{autotest_artifact}", autotest_artifact)
   end
 end
