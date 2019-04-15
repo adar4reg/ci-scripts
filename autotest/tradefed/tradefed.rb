87432cb784226912ccb9865471545617f8dea870
kuanlinchen <kuanlinchen@arimacomm.com.tw>
2019-04-15 16:13:26 +0800

    Modify scripts for running specific testcase
    Change-Id: I2293e559fd05943a2b4fb84e17a85e62fa771381

--- autotest/tradefed/tradefed.rb
+++ autotest/tradefed/tradefed.rb
@@ -14,27 +14,44 @@ def upload(url, file)
   RestClient.put url, File.open(file, 'rb'), {"X-Checksum": md5}
 end
 
+def run_tradefed(project, fastboot_artifact, autotest_artifact, testcase, flasher)
+  system("./tradefed.sh", "#{project}", "#{fastboot_artifact}", "#{autotest_artifact}", "#{testcase}", "#{flasher}")
+  if $?.exitstatus > 0
+    exit($?.exitstatus)
+  end
+end
+
 project = ARGV[0]
+pattern = ARGV[1]
+testcase = ARGV[2]
+flasher = ARGV[3]
 
 base_url = "http://artifactory.arimacomm.com.tw:8081/artifactory"
 aql = 'items.find({"$and":[{"created":{"$last":"2days"}},{"name":{"$match":"' + project + '*userdebug*fastbootimage.7z"}}]})'
 result = RestClient.post "#{base_url}/api/search/aql", aql, :content_type => "text/plain"
 files = JSON.parse(result.to_s)["results"]
 
-files.each do |file|
-  begin
-    fastboot_artifact = file['name']
-    autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'autotest.zip')
-    RestClient.get "#{base_url}/api/storage/libs-test-local/#{file['path']}/#{autotest_artifact}"
-  rescue    
-    download("#{base_url}/#{file['repo']}/#{file['path']}/#{fastboot_artifact}", fastboot_artifact)
-
-    system("./tradefed.sh", "#{project}", "#{fastboot_artifact}", "#{autotest_artifact}")
-
-    if $?.exitstatus > 0
-      exit($?.exitstatus)
+if pattern.nil? || pattern.empty?
+  files.each do |file|
+    begin
+      fastboot_artifact = file['name']
+      autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'autotest.zip')
+      RestClient.get "#{base_url}/api/storage/libs-test-local/#{file['path']}/#{autotest_artifact}"
+    rescue
+      download("#{base_url}/#{file['repo']}/#{file['path']}/#{fastboot_artifact}", fastboot_artifact)
+      run_tradefed("#{project}", "#{fastboot_artifact}", "#{autotest_artifact}", "#{testcase}", "#{flasher}")
+      upload("#{base_url}/libs-test-local/#{file['path']}/#{autotest_artifact}", autotest_artifact)
     end
-    
-    upload("#{base_url}/libs-test-local/#{file['path']}/#{autotest_artifact}", autotest_artifact)
+  end
+else
+  begin
+    path = pattern.rpartition('/').first.rpartition('local/').last
+    fastboot_artifact = pattern.rpartition('/').last
+    autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', testcase + '.zip')
+    RestClient.get "#{base_url}/api/storage/libs-test-local/#{path}/#{autotest_artifact}"
+  rescue
+    download("#{base_url}/#{pattern}", fastboot_artifact)
+    run_tradefed("#{project}", "#{fastboot_artifact}", "#{autotest_artifact}", "#{testcase}", "#{flasher}")
+    upload("#{base_url}/libs-test-local/#{path}/#{autotest_artifact}", autotest_artifact)
   end
 end
--- autotest/tradefed/tradefed.sh
+++ autotest/tradefed/tradefed.sh
@@ -3,6 +3,15 @@
 project=$1
 fastboot_artifact=$2
 autotest_artifact=$3
+testcase=$4
+flasher=$5
+
+if [ -z "$testcase" ]; then
+  testcase=$project
+fi
+if ! [ -z "$flasher" ]; then
+  flasher="--flasher-class $flasher"
+fi
 
 rm -rf tmp
 mkdir tmp
@@ -11,7 +20,7 @@ mv ../$fastboot_artifact .
 7z x $fastboot_artifact
 zip -j fastboot.zip fastboot_bin/*
 
-java -cp "/opt/tradefed/*" com.android.tradefed.command.CommandRunner arima/${project} --serial=${project}AAAAAAAAAAA
+java -cp "/opt/tradefed/*" com.android.tradefed.command.CommandRunner arima/${testcase} --serial=${project}AAAAAAAAAAA ${flasher}
 
 for z in 0/stub/**/*.zip; do unzip ${z}; done
 mv 0/stub/**/test*.html test_result.html
