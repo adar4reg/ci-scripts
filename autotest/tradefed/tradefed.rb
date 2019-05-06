2bc691b5d0ee2ddefe446e09afc932d55b825aef
kuanlinchen <kuanlinchen@arimacomm.com.tw>
2019-05-06 08:57:18 +0800

    Upload results to original path and update properties
    Change-Id: Ie1bd0ef9a1d4409a688b49e6014bdc238bd4546e

--- autotest/tradefed/tradefed.rb
+++ autotest/tradefed/tradefed.rb
@@ -21,6 +21,24 @@ def run_tradefed(project, serial, fastboot_artifact, autotest_artifact, testcase
   end
 end
 
+def get_times(base_url, file)
+  times = 0
+  payload = 'items.find({"$and":[{"name":{"$match":"'+file+'"}}]}).include("property.*")'
+  prop = RestClient.post "#{base_url}/api/search/aql", payload, :content_type => "text/plain"
+  properties = JSON.parse(prop.to_s)["results"][0]["properties"]
+  properties.each do |prop|
+    if prop["key"].eql? "test.times"
+      times = prop["value"].to_i
+    end
+  end
+  return times
+end
+
+def update_prop(base_url, pattern, status, times)
+  payload = '{"props":{"test.status":"'+status+'","test.times":"'+times+'"}}'
+  RestClient.patch "#{base_url}/api/metadata/#{pattern}", payload, :content_type => "application/json"
+end
+
 project = ARGV[0]
 serial = ARGV[1]
 pattern = ARGV[2]
@@ -37,23 +55,24 @@ if pattern.nil? || pattern.empty?
     begin
       fastboot_artifact = file['name']
       autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'autotest.zip')
-      RestClient.get "#{base_url}/api/storage/libs-test-local/#{file['path']}/#{autotest_artifact}"
+      RestClient.get "#{base_url}/api/storage/#{file['repo']}/#{file['path']}/#{autotest_artifact}"
     rescue
       download("#{base_url}/#{file['repo']}/#{file['path']}/#{fastboot_artifact}", fastboot_artifact)
       run_tradefed("#{project}", "#{serial}", "#{fastboot_artifact}", "#{autotest_artifact}", "#{testcase}", "#{flasher}")
-      upload("#{base_url}/libs-test-local/#{file['path']}/#{autotest_artifact}", autotest_artifact)
+      upload("#{base_url}/#{file['repo']}/#{file['path']}/#{autotest_artifact}", autotest_artifact)
+      update_prop(base_url, "#{file['repo']}/#{file['path']}/#{fastboot_artifact}", "completed", (get_times(base_url, fastboot_artifact)+1).to_s)
     end
   end
 else
-  begin
-    path = pattern.rpartition('/').first.rpartition('local/').last
-    fastboot_artifact = pattern.rpartition('/').last
-    autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'test-' + Time.now.strftime("%H%M") + '.zip')
-    RestClient.get "#{base_url}/api/storage/libs-test-local/#{path}/#{autotest_artifact}"
-  rescue
-    system("ruby", "configuration.rb", "#{testcase}")
-    download("#{base_url}/#{pattern}", fastboot_artifact)
-    run_tradefed("#{project}", "#{serial}", "#{fastboot_artifact}", "#{autotest_artifact}", "../test.xml", "#{flasher}")
-    upload("#{base_url}/libs-test-local/#{path}/#{autotest_artifact}", autotest_artifact)
-  end
+  # set up variables
+  path = pattern.rpartition('/').first.rpartition('local/').last
+  fastboot_artifact = pattern.rpartition('/').last
+  times = (get_times(base_url, fastboot_artifact)+1).to_s
+  autotest_artifact = fastboot_artifact.sub('fastbootimage.7z', 'test' + times + '.zip')
+  # start testing
+  system("ruby", "configuration.rb", "#{testcase}")
+  download("#{base_url}/#{pattern}", fastboot_artifact)
+  run_tradefed("#{project}", "#{serial}", "#{fastboot_artifact}", "#{autotest_artifact}", "../test.xml", "#{flasher}")
+  upload("#{base_url}/libs-test-local/#{path}/#{autotest_artifact}", autotest_artifact)
+  update_prop(base_url, pattern, "completed", times)
 end
