13fa1c478a6fb7db91e935f504436f6c6f50e1be
kuanlinchen <kuanlinchen@arimacomm.com.tw>
2019-06-06 16:24:04 +0800

    Add scripts to set up device

---
+++ autotest/tradefed/config/8710_monkey_config.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created By   : Dick Tsai
+# Created Date : Mon May 27 2019
+# Description  : Set up device before running monkey test
+
+import sys
+import time
+from uiautomator import Device
+
+d = Device(sys.argv[1])
+d.screen.on()
+
+d(resourceId="com.yandex.setupwizard:id/btn_language").click.wait()
+d(text="English (United States)").click.wait()
+
+d(text="Get started").wait.exists(timeout=30)
+
+# language
+d(text="Get started").click.wait()
+
+# connect to mobile network
+d(text="SKIP").click.wait()
+
+# connect to wi-fi
+d(text="SKIP").click.wait()
+d(text="CONTINUE").click.wait()
+
+# date & time
+d(text="NEXT").click.wait()
+
+# fingerprint
+d(text="SKIP").click.wait()
+
+# protect your phone
+d(text="Not now").click.wait()
+d(text="SKIP ANYWAY").click.wait()
+
+# google services
+d(text="MORE").click.wait()
+d(text="ACCEPT").click.wait()
+
+# accelerated location
+d(text="Next").click.wait()
+
+# open settings
+d(resourceId="com.google.android.googlequicksearchbox:id/search_widget_google_logo").wait.exists(timeout=30)
+d.open.quick_settings()
+d(resourceId="com.android.systemui:id/settings_button").click.wait()
+
+# turn off WiFi
+d(scrollable=True).scroll.to(text="Network & internet")
+d(text="Network & internet").click.wait()
+if d(text="ON", className="android.widget.Switch").exists:
+   d(text="ON", className="android.widget.Switch").click.wait()
+d.press("back")
+
+# turn off bt
+d(scrollable=True).scroll.to(text="Connected devices")
+d(text="Connected devices").click.wait()
+d(text="Connection preferences").click.wait()
+d(text="Bluetooth").click.wait()
+if d(text="ON", className="android.widget.Switch").exists:
+   d(text="ON", className="android.widget.Switch").click.wait()
+d.press("back")
+d.press("back")
+d.press("back")
+
+# Display -> Choose "Never"
+d(scrollable=True).scroll.to(text="Display")
+d(text="Display").click.wait()
+d(text="Advanced").click.wait()
+d(text="Sleep").click.wait()
+d(text="30 minutes").click.wait()
+d.press("back")
+
+# Security -> Choose Screen lock "None"
+d(scrollable=True).scroll.to(text="Security & location")
+d(text="Security & location").click.wait()
+d(text="Screen lock").click.wait()
+d(text="None").click.wait()
+d.press("back")
+
+# Stay Awake > On
+d(scrollable=True).scroll.to(text="System")
+d(text="About phone").click.wait()
+d(scrollable=True).scroll.to(text="Build number")
+d(text="Build number").click()
+d(text="Build number").click()
+d(text="Build number").click()
+d(text="Build number").click()
+d(text="Build number").click()
+d(text="Build number").click()
+d(text="Build number").click()
+d.press("back")
+d(text="System").click.wait()
+d(text="Advanced").click.wait()
+d(text="Developer options").click.wait()
+d(scrollable=True).scroll.to(text="Stay awake")
+d(text="Stay awake").right(text="OFF", className="android.widget.Switch").click.wait()
+d.press("home")
