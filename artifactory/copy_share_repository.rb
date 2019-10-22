e597a0235c4ef388b435bcc2eb6fb9e98da58e93
yungwenpeng(55451) <yungwenpeng@arimacomm.com.tw>
2019-10-22 11:17:41 +0800

    Refactor code for argument
    Change-Id: I9cdc4f8064762e11de7f2d6520d009ba170f76af

--- artifactory/copy_share_repository.rb
+++ artifactory/copy_share_repository.rb
@@ -1,98 +1,50 @@
 require 'json'
-require 'optparse'
+require 'optimist'
 require 'rest-client'
 
-options = {}
-option_parser = OptionParser.new do |parser|
-  parser.on("-p PROJECT", "--project=p", /\A[^\-]+/, "Require the project for query artifactory files") do |project|
-    options[:project] = project
-  end
-  parser.on("-t TIMESTAMP", "--timestamp=t", /\A[^\-]+/, "Require the timestamp for query artifactory files") do |timestamp|
-    options[:timestamp] = timestamp
-  end
-  parser.on("-u USERNAME", "--username=u", /\A[^\-]+/, "Require the username for query artifactory files") do |username|
-    options[:username] = username
-  end
-  parser.on("-w PASSWORD", "--password=w", /\A[^\-]+/, "Require the password for query artifactory files") do |password|
-    options[:password] = password
-  end
-  parser.on("-s SOURCEREPO", "--srcRepoKey=c", /\A[^\-].*/, "Require repository name which had been copied from") do |srcRepoKey|
-    options[:srcRepoKey] = srcRepoKey
-  end
-  parser.on("-c DESTINATIONREPO", "--dstRepoKey=c", /\A[^\-].*/, "Require repository name which had been copied to") do |dstRepoKey|
-    options[:dstRepoKey] = dstRepoKey
-  end
-  parser.on("-f FILETYPE", "--filetype=f", /\A[^\-]+/, "Require the filetype for query artifactory files") do |filetype|
-    options[:filetype] = filetype
-  end
-  parser.on("-d DRYRUN", "--dryrun=c", /\A[^\-]+/, "Require dry-run for this build") do |dryrun|
-    options[:dryrun] = dryrun
-  end
+options = Optimist::options do
+  opt :project, 'Require the project for query artifactory files', :type => :string, :required => true
+  opt :timestamp, 'Require the timestamp for query artifactory files', :type => :string, :required => true
+  opt :username, 'Require the username for query artifactory files', :type => :string, :required => true
+  opt :password, 'Require the password for query artifactory files', :type => :string, :required => true
+  opt :srcRepoKey, 'Require repository name which had been copied from', :type => :string, :default => 'libs-staging-local', :required => true
+  opt :dstRepoKey, 'Require repository name which had been copied to', :type => :string, :required => true
+  opt :filetype, 'Require the filetype for query artifactory files', :type => :string, :required => true
+  opt :dryrun, 'Require dry-run for this build', :type => :string, :default => '1', :required => true
 end
 
+puts options
 
-begin
-  ARGV << "-h" if ARGV.size < 1
-  option_parser.parse!(ARGV)
-  raise OptionParser::MissingArgument if options[:project].nil? or options[:timestamp].nil? or options[:username].nil? or options[:password].nil? or options[:dstRepoKey].nil?
-  options[:filetype] = "fastboot" if options[:filetype].nil?
-  options[:dryrun] = "1" if options[:dryrun].nil?
-rescue OptionParser::ParseError
-  if $!.to_s =~ /invalid\s+argument\:\s+(\-\S+)\s+\-/ or $!.to_s =~ /missing argument/
-    $stderr.print "Error: missing argument value: #{$1}\n"
-  else
-    $stderr.print "Error: " + $!.to_s + "\n"
-  end
-  puts "#{option_parser}"
-  exit
-end
+options[:filetype] = 'all' if options[:dstRepoKey] == 'libs-release-local'
+options[:filetype] = options[:filetype] == 'all' ? '' : (options[:filetype] + '*')
+query_name = "#{options[:project]}*#{options[:timestamp]}*#{options[:filetype]}"
 
-puts "project: #{options[:project]}, timestamp: #{options[:timestamp]},srcRepoKey: #{options[:srcRepoKey]}, dstRepoKey: #{options[:dstRepoKey]}"
-puts "filetype: #{options[:filetype]}, dryrun: #{options[:dryrun]}"
-
-project = options[:project]
-timestamp = options[:timestamp]
-username = options[:username]
-password = options[:password]
-srcRepoKey = options[:srcRepoKey]
-dstRepoKey = options[:dstRepoKey]
-filetype = options[:filetype]
-dryrun = options[:dryrun]
-query_type = "name"
-query_name = "#{project}*#{timestamp}*"
-query_operation = "or"
-
-filetype = "all" if dstRepoKey == "libs-release-local"
-
-if filetype == "all"
-  filetype = ""
-else
-  filetype = filetype + "*"
-end
-
-query = "items.find({
-  \"$and\":
-  [
-    {\"repo\":{\"$eq\":\"#{srcRepoKey}\"}},
-    {\"$#{query_operation}\":[
-      {\"#{query_type}\" : {\"$match\" : \"#{query_name}#{filetype}\"}}
-    ]}
+query = {
+  '$and': [
+    {'repo': {'$eq': options[:srcRepoKey]}},
+    {'name': {'$match': query_name}}
   ]
-})"
+}
 
-def copy_file(src_file, dest_file, dryrun, username, password)
-  RestClient.post "http://#{username}:#{password}@artifactory.arimacomm.com.tw:8081/artifactory/api/copy/#{src_file}?to=/#{dest_file}&dry=#{dryrun}", {produces: "application/vnd.org.jfrog.artifactory.storage.CopyOrMoveResult+json"}
+puts JSON[query]
+query = "items.find(#{JSON[query]})"
+
+
+def copy_file(src_file, dest_file, dryrun, api_base)
+  RestClient.post "#{api_base}/copy/#{src_file}?to=/#{dest_file}&dry=#{dryrun}",
+      {produces: 'application/vnd.org.jfrog.artifactory.storage.CopyOrMoveResult+json'}
 end
 
 
 ### query file
-results = RestClient.post "http://#{username}:#{password}@artifactory.arimacomm.com.tw:8081/artifactory/api/search/aql", query, {content_type: "text/plain"}
+api_base = "http://#{options[:username]}:#{options[:password]}@artifactory.arimacomm.com.tw:8081/artifactory/api"
+results = RestClient.post "#{api_base}/search/aql", query, {content_type: 'text/plain'}
 (JSON.parse results.to_s)["results"].each { |result|
-  repo = result["repo"]
-  path = result["path"]
-  name = result["name"]
+  repo = result['repo']
+  path = result['path']
+  name = result['name']
   src_file = "#{repo}/#{path}/#{name}"
-  dest_file = "#{dstRepoKey}/#{path}/#{name}"
-  puts "src_file  : #{src_file}\ndest_file : #{dest_file}"
-  puts copy_file("#{src_file}", "#{dest_file}", "#{dryrun}", "#{username}", "#{password}")
+  dest_file = "#{options[:dstRepoKey]}/#{path}/#{name}"
+
+  puts copy_file("#{src_file}", "#{dest_file}", "#{options[:dryrun]}", "#{api_base}")
 }
